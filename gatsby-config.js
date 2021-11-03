a3d2d80c4573d65630e260ad70a358243d5e55b2
hideakiyamamoto <61575280+ideahy@users.noreply.github.com>
2021-11-04 01:21:06 +0900

    gatsby-plugin-netlify-cmsを追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `エンジニアブログ`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Hideaki Yamamoto`,
+      summary: `京都に住んでいます`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `ギャツビーでエンジニアのブログを作ります。`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
       twitter: `kylemathews`,
